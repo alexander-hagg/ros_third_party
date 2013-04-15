f85b36b361e451d5ddac65c26ea1cefaa075a646
Midaz <info@haggdesign.de>
2013-04-15 15:13:23 +0200

    changed tar unpacking

--- ros_opencv_2_4_4/download_unpack_build.mk
+++ ros_opencv_2_4_4/download_unpack_build.mk
@@ -4,7 +4,7 @@ TARBALL = build/OpenCV-2.4.4a.tar.bz2
 TARBALL_URL = http://surfnet.dl.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.4/OpenCV-2.4.4a.tar.bz2
 SOURCE_DIR = build/opencv-2.4.4
 MD5SUM_FILE = OpenCV-2.4.4a.tar.bz2.md5sum
-UNPACK_CMD = tar xzf
+UNPACK_CMD = tar xjf
 include $(shell rospack find mk)/download_unpack_build.mk
 
 opencv: $(SOURCE_DIR)/unpacked
